efd4d3ac41ee0abe5efcee79b6318755d4d6fa18
Muzaffer <py.muzaffer.yardimci@gmail.com>
2024-06-12 23:22:32 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/Controller/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineMuhasebeServer.Presentation.Abstraction;
+
+namespace OnlineMuhasebeServer.Presentation.Controller
+{
+    public sealed class TestController : ApiController
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("İslem Basarılı");
+        }
+    }
+}
